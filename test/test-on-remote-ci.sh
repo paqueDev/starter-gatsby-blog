f2e11812e8480ebf17b7f8a9d6906d3defd70942
Benedikt Rötsch <info@benedikt-roetsch.de>
2018-08-07 15:17:10 +0200

    first try for remote ci call :yay:

---
+++ test/test-on-remote-ci.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Run end 2 end tests without polluting this repository
+
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+  echo "Not a pull request, skipping e2e tests"
+  exit 0
+fi
+
+ENV='TEST_REPO="contentful/starter-gatsby-blog" TEST_BRANCH="'$TRAVIS_BRANCH'" TEST_CMD_BUILD="sh ../tests/starter-gatsby-blog/build-repo.sh" CYPRESS_BASE_URL="http://localhost:9000" CYPRESS_INTEGRATION_FOLDER="tests/starter-gatsby-blog/integration"'
+
+BODY="{
+  \"request\": {
+    \"message\": \"$TRAVIS_REPO_SLUG SDK Triggered Request\",
+    \"branch\":\"travis_experiments\",
+    \"config\": {\"env\": $ENV}
+  }
+}"
+
+# api-coverage
+curl -s -X POST \
+	-d "$BODY" \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json"   \
+    -H "Travis-API-Version: 3"   \
+    -H "Authorization: token $TRAVIS_E2E_TOKEN \
+    'https://api.travis-ci.com/repo/1336919/requests'
